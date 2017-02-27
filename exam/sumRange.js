5de072883ddb67ec270c058ad6fb7d88cfcd1e47
Liu7 <liuqi0702@126.com>
2017-02-27 23:18:00 +0800

    首页修改！触摸事件，sumRange

--- exam/sumRange.js
+++ exam/sumRange.js
@@ -1,4 +1,10 @@
-var arr = [1, 3, -1, 59, 58, 12, 56, 789, 456, 554];
+var timeStart=new Date().getTime();
+var arr=[];
+for (var i=0;i<100000;i++){
+    arr.push(parseInt(Math.random()*10));
+}
+console.log('数组生成用时：',new Date().getTime()-timeStart);
+
 var memory = [];
 function sumTo(n) {
     if (memory[n]) {
@@ -9,14 +15,37 @@ function sumTo(n) {
         memory.push(arr[0]);
         return arr[0];
     }
-    console.log('sumTo n:',n);
     var sumX = arguments.callee(n - 1) + arr[n];
     memory.push(sumX);
     return sumX;
 }
 function sumRange(m, n) {
-    return m ? sumTo(n) - sumTo(m - 1) : sumTo(n)
+    return m>1 ? sumTo(n-1) - sumTo(m - 2) : sumTo(n-1)
 }
-console.log(sumRange(1, 5));
-console.log(memory);
-console.log(sumRange(2, 4));
+
+function sumRangeFor(m,n){
+    var s=0;
+    for(var i=m-1;i<n;i++){
+        s+=arr[i];
+    }
+    return s;
+}
+var time1=new Date().getTime();
+var s1=sumRangeFor(3, 100000);
+console.log('使用for循环计算3-10000000个元素相加',s1);
+console.log('用时：',new Date().getTime()-time1);
+
+var time2=new Date().getTime();
+var s2=sumRange(3, 100000);
+console.log('使用sumRange计算3-10000000个元素相加',s2);
+console.log('用时：',new Date().getTime()-time2);
+
+var time3=new Date().getTime();
+var s3=sumRangeFor(2, 100000);
+console.log('使用for循环计算2-10000000个元素相加',s3);
+console.log('用时：',new Date().getTime()-time3);
+
+var time4=new Date().getTime();
+var s4=sumRange(2, 100000);
+console.log('使用sumRange计算2-10000000个元素相加',s4);
+console.log('用时：',new Date().getTime()-time4);
