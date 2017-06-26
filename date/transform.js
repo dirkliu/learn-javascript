f917676939b29cd058802573249714ec333a9d6d
dirkliu <liuqi0702@126.com>
2017-06-26 20:14:06 +0800

    date ex

---
+++ date/transform.js
@@ -0,0 +1,5 @@
+//date transform to YYYY-mm-dd
+new Date().toLocaleString('zh-CN', {hour12: false}).replace(/\//g, '-').replace(/\b\d\b/g, '0$&').replace(/\s(\d{1,2}\:)*\d{1,2}/, '')
+
+// date transform YYYY-mm-dd HH:i:s
+new Date().toLocaleString('zh-CN', {hour12: false}).replace(/\//g, '-').replace(/\b\d\b/g, '0$&')
