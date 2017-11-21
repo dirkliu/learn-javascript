0c84d31e7b1074b16b9d8d94196ab4f91c9533f2
dirkliu <liuqi0702@126.com>
2017-11-21 10:27:33 +0800

    reg src

--- regexp/main.js
+++ regexp/main.js
@@ -2,5 +2,6 @@ var text = "cat, bat, sat, fat";
 var result = text.replace(/(.at)/g, "($1) word");
 console.log('result:', result)
 var urlx = "asdfa src=\"abc.img\"";
-var urlRes = urlx.replace(/(src=\"\.+\")/g, "$1axc")
+var timeStr = new Date().valueOf();
+var urlRes = urlx.replace(/src=\"(.+)"/g, "src=\"$1?" + timeStr +"\"")
 console.log('urlRes:', urlRes)
