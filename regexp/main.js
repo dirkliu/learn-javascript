9329ad08d0755339bc62a4cbb871d7cf61e09858
dirkliu <liuqi0702@126.com>
2018-03-06 10:48:52 +0800

    Get query by name!

--- regexp/main.js
+++ regexp/main.js
@@ -5,3 +5,11 @@ var urlx = "asdfa src=\"abc.img\"";
 var timeStr = new Date().valueOf();
 var urlRes = urlx.replace(/src=\"(.+)"/g, "src=\"$1?" + timeStr +"\"")
 console.log('urlRes:', urlRes)
+
+console.log('---获取查询字符串参数----')
+function getQuery (name) {
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+  var r = window.location.search.substr(1).match(reg);
+  if (r!=null) return unescape(r[2]); return null;
+}
+console.log(getQuery('test'))
