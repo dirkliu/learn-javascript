6e22c39a59d76f604451f48999d2456ef7d7ab7e
dirkliu <liuqi0702@126.com>
2018-10-28 22:38:43 +0800

    tree search: 扁平化，搜索。

--- tree/search.js
+++ tree/search.js
@@ -46,14 +46,85 @@ var list = [
   }, {
     id: 3,
     parentId: 0,
-    name: '中国'
+    name: '中国',
+
+    children: [
+      {
+        id: 31,
+        name: '北京',
+        parentId: 3
+      }, {
+        id: 32,
+        name: '吉林省',
+        parentId: 3,
+
+        children: [
+          {
+            id: 321,
+            parentId: 32,
+            name: '长春'
+          }, {
+            id: 322,
+            parentId: 32,
+            name: '吉林'
+          }
+        ]
+      }, {
+        id: 33,
+        name: '湖南省',
+        parentId: 3,
+
+        children: [
+          {
+            id: 331,
+            parentId: 332,
+            name: '长沙'
+          }, {
+            id: 332,
+            parentId: 32,
+            name: '益阳'
+          }
+        ]
+      }
+    ]
   }
 ]
 
-function search () {
+// 扁平化
+function flat (treeList) {
+  let _list = []
 
+  function _flat (treeList, listStack) {
+    treeList.forEach(item => {
+      let newItem = Object.assign({}, item)
+      delete newItem.children
+      listStack.push(newItem)
+
+      if (item.children) {
+        _flat(item.children, listStack)
+      }
+    })
+  }
+
+  _flat(treeList, _list)
+  return _list
+}
+
+function search (param, list) {
+  let flatList = flat(list)
+  return flatList.filter(item => item.name.indexOf(param) > -1)
 }
 
 function treeSearch (param, list) {
+  let treeSearchList = []
+  let searchList = search(param, list)
+
+  return treeSearchList
+}
+
+function traceback (node, nodeList) {
 
 }
+
+let searchList = search('秋田', list)
+console.log('searchList:', searchList)
