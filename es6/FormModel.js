8880b4c78b135e641456a3cddbe6bddd41af93bc
dirkliu <liuqi0702@126.com>
2018-12-10 01:25:58 +0800

    bugfix

--- es6/FormModel.js
+++ es6/FormModel.js
@@ -4,27 +4,40 @@ class FormModel {
 
   constructor (options) {
     Object.assign(this, options)
+    this._setObserve(options)
   }
 
-  setDefaults (options) {
+  _setObserve (options) {
+    !this._observeKeys && this._observeKeys = []
     Object.keys(options).forEach(key => {
+      this._observeKeys(key)
       Object.defineProperty(this, key, {
         set (value) {
           this['_' + key] = value || options[key]
         },
         get () {
-          console.log('key:', key)
+          // console.log('key:', key)
           return this['_' + key] || options[key]
         }
       })
     })
+  }
+
+  setDefaults (options) {
+    // Object.assign(this, options)
+    Object.keys(options).forEach(key => {
+      Object.assign(this, {
+        key: this[key] || options[key]
+      })
+    })
+    this._setObserve(options)
     this.defaults = options
     return this
   }
 }
 
 let formData = new FormModel({
-  ids: ''
+  ids: '176'
 })
 
 formData.setDefaults({
